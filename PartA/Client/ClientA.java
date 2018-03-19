157f576b7c33a7a4b24bac1bd65e7aa66907c51f
JulianAppleyard <34401986+JulianAppleyard@users.noreply.github.com>
2018-03-19 19:40:36 +0000

    0.5 Part A 60%ish percent done
    Part B not started (is dependent on partA)
    Appear to be operating properly:
    CONN
    LIST
    DELF
    QUIT
    Partially working:
    UPLD
    Not implemented
    DWNLD

---
+++ PartA/Client/ClientA.java
@@ -0,0 +1,363 @@
+/**
+* The client-side code of a simple FTP application.
+* Author: Julian Appleyard
+* Version: 0.5
+Version notes:
+* CONN LIST DELF QUIT are all working as expected (so far)
+* UPLD behaves improperly when using a file larger than a text file
+*
+* Planned structure is that each command will call a method that mirrors a method executed on the server.
+* The server and client methods will be structured in way that they wait for each other's responses at the appropriate time
+* so that they do not become out of sync.
+**/
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.ByteBuffer;
+import java.net.*;
+import java.util.Scanner;
+import java.lang.*;
+import static java.lang.Math.toIntExact;
+//import org.apache.commons.net.ftp.*; //Using Apache Commons Net API
+
+public class ClientA{
+
+//  private static FTPClient ftp;
+  private static Socket socket;
+  private static OutputStream outToServer;
+
+
+  public ClientA(String host, int port){
+
+    try{
+      System.out.println("Connecting to " + host + " on port " + port + "..."); //debug
+      socket = new Socket(host, port);
+
+      System.out.println("Connected"); //debug
+    } catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+/* Method for uploading a File to the Server
+
+*/
+///*
+  public static void uploadToServer(String filePath){
+
+    try{
+      Path p = Paths.get(filePath);
+
+      File fileToBeUploaded = p.toFile();
+      String file_name = p.getFileName().toString(); //throws FileNotFoundException
+      System.out.println("DEBUG FILENAME: " +file_name);
+      int fileNameLength = file_name.length();
+      short fileNameShortLength = (short) fileNameLength;
+      //There is a max length that this will work for do I need to account for this?
+
+      System.out.println("File namelength is: " + fileNameLength); //debug
+      System.out.println("Length in Short is: " + fileNameShortLength); //debug
+      // Outline wants the length of the file name to be sent in short int
+      //short shortNameLength = fileNameLength;
+      //send name and length of name (in short)
+
+
+
+      DataOutputStream dataOut = new DataOutputStream(outToServer);
+
+
+      //need to send length and name in one go
+      // convert to byte array with first two values being the length in short
+      ByteBuffer bb = ByteBuffer.allocate(2);
+      bb.putShort(fileNameShortLength);
+      byte[] lengthArray = bb.array();
+      //encode the file name in second byte array using UTF-8
+      byte[] nameArray = file_name.getBytes("UTF-8");
+      System.out.println(lengthArray.length);//debug
+      System.out.println(nameArray.length);//debug
+
+      //concatenate the two arrays
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      baos.write(lengthArray);
+      baos.write(nameArray);
+      byte[] outArray = baos.toByteArray();
+
+      System.out.println("Sending byteArray...");//DEbug
+
+      //send the concatenated byte array to the server
+      dataOut.write(outArray, 0, outArray.length);
+
+
+
+      //Now wait for ack from server that it received the file name and name length
+      InputStream iStream = socket.getInputStream();
+      InputStreamReader isr = new InputStreamReader(iStream);
+      BufferedReader br = new BufferedReader(isr);
+      System.out.println("reading ack");
+      String response = br.readLine();
+
+      System.out.println("ack was: " + response);
+
+
+      System.out.println("ack received from server");//debug
+
+
+      //Client sends the size of the file, which maay be a 32 bit value sent in bytes
+      long longFileSize = fileToBeUploaded.length();
+      int intFileSize = toIntExact(longFileSize); //Will throw an ArithmeticException in case of overflow
+      System.out.println("Int filesize: "+ intFileSize);
+      ByteBuffer sizeBuff = ByteBuffer.allocate(4);
+      sizeBuff.putInt(intFileSize);
+      byte[] sizeArray = sizeBuff.array();
+
+      dataOut.write(sizeArray, 0, sizeArray.length);
+
+      //send file
+      //convert file into byte array
+      FileInputStream fileStream = new FileInputStream(fileToBeUploaded);
+      byte[] fileBytes = new byte[intFileSize];
+      fileStream.read(fileBytes);
+
+      //FileInputStream reads files as bytes and DataOutputStream writes bytes to server
+      dataOut.write(fileBytes, 0, fileBytes.length);
+
+      String progress = br.readLine();
+      System.out.println(progress);
+
+    }catch(NullPointerException e){
+      e.printStackTrace();
+    }
+    catch(FileNotFoundException e){
+      System.out.println("File not found.");
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+//  */
+  /*
+  public static void downloadFromServer(){
+    //send length of file name in short int followed by file file_name
+
+    //
+  }
+  */
+
+  public static void listFilesOnServer(){
+    try{
+      InputStream iStream = socket.getInputStream();
+      DataInputStream dataIn = new DataInputStream(iStream);
+
+      //Server sends the size of directory listing to client as 32bit int
+
+      byte[] sizeArray = new byte[4];
+      dataIn.read(sizeArray);
+      ByteBuffer sizeBuff = ByteBuffer.wrap(sizeArray);
+      int size = sizeBuff.getInt(0);
+
+      System.out.println("DEBUG size of directory listing: " + size); //debug
+
+      //Client receives the size and goes into a loop to read directory listing
+      //
+      System.out.println("DEBUG waiting for directory listing itself...");//debug
+      byte[] listingArray = new byte[size*4]; //individual characters are encoded with 1 to 4 bytes in UTF-8
+      dataIn.read(listingArray);
+
+      //decode byte array to string using UTF-8
+      String names = new String(listingArray, "UTF-8");
+      names = names.trim();
+
+      //find separate filenames around the "/" character, which is in no filenames as it is not allowed by UNIX and Windows systems
+      String[] directoryListing = names.split("/");
+
+      System.out.println("\n");
+      System.out.println("There are " +directoryListing.length + " files in the server's storage:");
+      for(int i = 0; i<directoryListing.length; i++){
+        System.out.println(directoryListing[i]);
+      }
+      System.out.println("\n");
+
+
+
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
+/*
+  This method handles the DELF Function
+  The client sends the filename and
+*/
+
+  public static void deleteFileOnServer(String file_name){
+    try{
+      InputStream is = socket.getInputStream();
+      DataInputStream dataIn = new DataInputStream(is);
+      DataOutputStream dataOut = new DataOutputStream(outToServer);
+      int nameLengthInt = file_name.length();
+      short nameLengthShort = (short) nameLengthInt;
+
+      //"Client sends the length of the file name (short) followed by the file name"
+      ByteBuffer bb = ByteBuffer.allocate(2);
+      bb.putShort(nameLengthShort);
+
+      //Convert the two values into byte arrays
+      byte[] lengthArray = bb.array();
+      byte[] nameArray = file_name.getBytes("UTF-8");
+
+      //concatenate the two byte arrays
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      baos.write(lengthArray);
+      baos.write(nameArray);
+      byte[] outArray = baos.toByteArray();
+
+      System.out.println("Sending byteArray...");//debug
+
+      //send the concatenated byte array to the server
+      dataOut.write(outArray, 0, outArray.length);
+
+      //wait for integer confirmation code
+      byte[] confirmBytes = new byte[4];
+      dataIn.read(confirmBytes);
+      ByteBuffer inBuffer = ByteBuffer.wrap(confirmBytes);
+      int confirm = inBuffer.getInt();
+
+      String command = "";
+      byte[] commandBytes = new byte[0];
+      if(confirm == 1){
+        System.out.println("Are you sure you want to delete " + file_name + " from the server's storage?");
+        System.out.println("Enter 'yes' to delete or 'no' to abort deletion process");
+
+        while(true){
+          Scanner keyboard = new Scanner(System.in);
+          command = keyboard.nextLine().trim();
+
+          if(command.equalsIgnoreCase("yes")){
+            System.out.println("Confirming deletion with server...");//debug
+
+            commandBytes = command.getBytes();
+            dataOut.write(commandBytes, 0, commandBytes.length);
+            //wait for ack of deletion
+
+            byte[] ackBytes = new byte[50];
+            dataIn.read(ackBytes);
+            String ack = new String(ackBytes, "UTF-8");
+            System.out.println("Server responded with: " + ack);
+
+            break;
+          }
+          else if(command.equalsIgnoreCase("no")){
+
+            System.out.println("Delete abandoned by the user!");
+
+            commandBytes = command.getBytes();
+            dataOut.write(commandBytes, 0, commandBytes.length);
+            break;
+          }
+          else{
+            System.out.println("Invalid response. Enter 'yes' to delete the file or 'no' to abort deletion process");
+          }
+          //The client thne sends the user's confirm ("yes" or "no") back to the server
+
+        }
+
+      }else{
+        System.out.println("The file does not exist on server");
+        //returns to "prompt user for operation" state
+      }
+
+
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String [] args){
+    String host = "localhost";
+    int port = 42000;
+
+    try{
+
+      ClientA client = new ClientA(host, port);
+
+      outToServer = socket.getOutputStream();
+
+      OutputStreamWriter osw = new OutputStreamWriter(outToServer);
+      BufferedWriter bw = new BufferedWriter(osw);
+
+      Boolean isTerminated = false;
+
+      while(true){
+
+        //Use Scanner to record user input
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter message to send to " + host);
+        String textToServer = keyboard.nextLine();
+
+
+        bw.write(textToServer +"\n");
+        bw.flush();
+        //System.out.println(textToServer + " sent to server");
+        switch(textToServer){
+          case "HELP":
+            System.out.println("Valid commands are:");
+            System.out.println("UPLD : to upload a file to the server");
+            System.out.println("LIST : to list the directories/files at the server");
+            System.out.println("DWLD : to download a file from the server");
+            System.out.println("DELF : to delete a file from the server \n");
+            break;
+
+          case "UPLD":
+            System.out.println("Beginning procedure for uploading a file to the server...");
+
+            System.out.println("Enter path of file to be uploaded");
+            String path = keyboard.nextLine();
+            //call upload file method
+            uploadToServer(path);
+            break;
+
+          case "LIST":
+            System.out.println("Beginning procedure for listing files on the server...");
+            //call list files method
+            listFilesOnServer();
+            break;
+
+          case "DWLD":
+            System.out.println("Beginning procedure for downloading a file from the server...");
+            //call method to download a file from the server
+            break;
+
+          case "DELF":
+            System.out.println("Beginning procedure for deleting a file from the server...");
+            System.out.println("Enter the name of the file on the server that you want to delete");
+
+            String filename = keyboard.nextLine();
+
+            deleteFileOnServer(filename);
+            //call delete files method
+            break;
+
+          case "QUIT":
+            socket.close();
+            isTerminated = true;
+            break;
+
+          default:
+            System.out.println("Invalid command. Type 'HELP' to see valid commands.");
+        }
+        if(isTerminated){
+          break;
+        }
+      }//innner while
+    }//try
+    catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
+}
---
+++ PartA/Server/ServerA.java
@@ -0,0 +1,384 @@
+/**
+* The server-side code of a simple FTP application. The file transfer itself
+* should take place using TCP with the client.
+* Author: Julian Appleyard
+* Version: 0.5
+* see ClientA for version notes
+
+Should "invalid command" responses be handled Server-side or client-side?
+
+**/
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+
+public class ServerA {
+  private static Socket socket;
+  private static InputStream is;
+/*
+This method handles the UPLD function.
+The client sends the operation "UPLD" to upload a local file to the server.
+current this will overwrite files if they have the same name
+This doesnt work with larger files for some reason?
+
+*/
+
+  public static void uploadFromClient(){
+    try{
+        DataInputStream dataIn = new DataInputStream(is);
+
+
+        byte[] inArray = new byte[50];
+        //byte array has an arbitrary limited length
+        //is this bad practice?
+        dataIn.read(inArray);
+
+        System.out.println("Recieving file name...");//debug
+
+        //Client sends the length of the file name which will be sent (short int) and the file_name itself
+        // These will be sent encoded in a byte Array
+        // the first two values are the length of the name in type short
+        // the remaining will be the file_name itself encoded as UTF-8
+
+        ByteBuffer buffer = ByteBuffer.wrap(inArray);
+
+        short lengthShort = buffer.getShort(0);
+        System.out.println("DEBUG length: "+ lengthShort); //debug
+
+        //System.out.println(buffer.toString());//debug
+        byte[] temp = new byte[buffer.remaining()];
+        buffer.get(temp);
+
+        String file_name = new String(temp,"UTF-8");
+        file_name = file_name.trim();
+        System.out.println("DEBUG string: "+ file_name);//debug
+
+        //send acknowledgement that the server is ready to receive
+        OutputStream oStream = socket.getOutputStream();
+        OutputStreamWriter osr = new OutputStreamWriter(oStream);
+        BufferedWriter bw = new BufferedWriter(osr);
+
+        String message = "Send file size" + "\n";
+        System.out.println("Sending ack");//debug
+        bw.write(message, 0, message.length());
+        bw.flush();
+        System.out.println("Ack sent"); //debug
+
+        //Server receives and decodes file filesize
+
+        byte[] sizeArray = new byte[5];
+        dataIn.read(sizeArray);
+        ByteBuffer sizeBuff = ByteBuffer.wrap(sizeArray);
+        int fileSize = sizeBuff.getInt(0); //what do I do with this?
+
+
+        //ok so if the file is too big it will fuck everything up
+        System.out.println("filesize: " + fileSize);//debug
+
+        Path currentRelativePath = Paths.get("");
+        String stringPath = currentRelativePath.toAbsolutePath().toString();
+        File newFile = new File(stringPath + "\\Storage\\" + file_name);
+        //what if the file name already exists? make a copy?
+        System.out.println(newFile.getPath());//debug
+        newFile.createNewFile();
+
+        FileOutputStream fileOutputWriter = new FileOutputStream(newFile);
+        byte[] fileBytes = new byte[fileSize];
+        dataIn.read(fileBytes, 0, fileSize);
+
+        fileOutputWriter.write(fileBytes);
+        System.out.println("File written");
+        bw.write(fileBytes.length + " bytes written" + "\n");
+        bw.flush();
+
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+/*
+Method for listing files on the server
+Going to only list files in the "Storage" directory
+What exceptions are thrown?
+*/
+
+  public static void listFilesOnServer(){
+    try{
+
+      //Stream management
+      OutputStream os = socket.getOutputStream();
+      DataOutputStream listDataOut = new DataOutputStream(os);
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+      //Client sends operation (LIST) to list directories/files at the server
+      //Server obtains listing of its directories/files
+      //it is sufficient to only list the names of files/directories
+      Path currentRelativePath = Paths.get("");
+      String stringPath = currentRelativePath.toAbsolutePath().toString();
+      File directory = new File(stringPath + "\\Storage\\");
+      String[] listOfFileNames = directory.list();
+
+
+      //loop through the list of file names to find the size of the listing
+
+      int size = 0;
+      for(int i =0; i<listOfFileNames.length; i++){
+        //
+        int lengthOfCurrentString = listOfFileNames[i].length();
+        size = size + lengthOfCurrentString;
+
+        //convert each filename into a byte array (UTF-8 encoded)
+        //and write to the ByteArrayOutputStream
+        byte[] nameArray = listOfFileNames[i].getBytes("UTF-8");
+        baos.write(nameArray);
+        byte[] seperatorArray = "/".getBytes("UTF-8");
+        baos.write(seperatorArray);
+      }
+
+      //ByteArrayOutputStream baos will contain all filenames concatenated
+      //but separated with a "/" to allow for the client to parse the filenames
+      //I chose this character because it is not allowed in filenames in UNIX or Windows
+      //
+
+      byte[] listingArray = baos.toByteArray();
+
+
+      System.out.println("DEBUG size of directory listing: " + size);
+
+      //convert size of listing (32 bit int) to byte array for sending to client
+
+      ByteBuffer listBuffer = ByteBuffer.allocate(4);
+      listBuffer.putInt(size);
+      byte[] lengthArray = listBuffer.array();
+
+      System.out.println("Sending lengthArray...");//debug
+      listDataOut.write(lengthArray, 0, lengthArray.length);
+
+      System.out.println("Sending listingArray...");//debug
+      listDataOut.write(listingArray, 0, listingArray.length);
+
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+/*
+This method handles the DWNLD function.
+The client requests a file and the server sends the file to the client
+just an outline for now
+*/
+/*
+  public static void downloadToClient(){
+    try{
+
+    }Catch(){
+
+    }
+  }
+
+  */
+
+/*
+This method handles the DELF function
+The client sends the file name the server deletes file if it exists
+just an outline for now
+*/
+
+  public static void deleteFileOnServer(){
+    try{
+      OutputStream os = socket.getOutputStream();
+      DataOutputStream dataOut = new DataOutputStream(os);
+      DataInputStream dataIn = new DataInputStream(is);
+      byte[] inArray = new byte[50];
+
+      //Client sends the length of the file name (short int) and the filename (String)
+      dataIn.read(inArray);
+      System.out.println("Receiving file name...");//debug
+
+      ByteBuffer inBuffer = ByteBuffer.wrap(inArray);
+      /*
+        Server decodes file name size and file name
+      */
+
+      //The first two bytes are the file name length in short
+      //
+      short lengthShort = inBuffer.getShort(0);
+      System.out.println("Debug length: " + lengthShort);
+
+      //The remaining bytes are the file name String
+      //
+      byte[] nameBytes = new byte[inBuffer.remaining()];
+      inBuffer.get(nameBytes);
+
+      String file_name = new String(nameBytes, "UTF-8");
+      file_name = file_name.trim();
+      System.out.println("Debug file to be deleted: " + file_name);
+
+      Path currentRelativePath = Paths.get("");
+      String stringPath = currentRelativePath.toAbsolutePath().toString();
+      stringPath = stringPath + "\\Storage\\" + file_name;
+
+      File f = new File(stringPath);
+      System.out.println(stringPath);
+      System.out.println(f.exists());//debug
+
+      Boolean fileExists = f.exists();
+
+      //Server checks if the file to be deleted exists or not
+      if(fileExists){
+        //if the file does exist, the server sends a positive confirm (integer value 1)
+        int confirm = 1;
+        ByteBuffer outBuffer = ByteBuffer.allocate(4);
+        outBuffer.putInt(confirm);
+        byte[] outArray = outBuffer.array();
+        dataOut.write(outArray, 0, outArray.length);
+
+        byte[] commandBytes = new byte[50];
+        dataIn.read(commandBytes);
+
+        String command = new String(commandBytes, "UTF-8");
+        command = command.trim();
+
+        if(command.equalsIgnoreCase("yes")){
+          //if the confirm is yes the server deletes the requested file and returns an ack to the client
+          //to indicate the success or failure of file deletion
+          //
+          Boolean isSuccess = f.delete();
+          String ack = "";
+          if(isSuccess){
+            ack = "File deleted successfully";
+          }
+          else{
+            ack = "Deletion failed";
+          }
+          //send ack to client
+          byte[] ackBytes = ack.getBytes("UTF-8");
+          dataOut.write(ackBytes, 0, ackBytes.length);
+
+        }
+        else if(command.equalsIgnoreCase("no")){
+          //if the confirm is no, the server reutnrs to "Wait for operatio from client" state
+        }
+
+      }
+      else{
+        // If the file does not exist server sends a negative confirm (integer value -1)
+        int confirm = -1;
+        ByteBuffer outBuffer = ByteBuffer.allocate(4);
+        outBuffer.putInt(confirm);
+        byte[] outArray = outBuffer.array();
+        dataOut.write(outArray, 0, outArray.length);
+        System.out.println("File does not exist. Returning to main menu...");
+      }
+      //method exits to main menu
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
+  public static void main (String args[]){
+    try{
+
+      int port = 42000;
+      ServerSocket serverSocket = new ServerSocket(port);
+      System.out.println("Server started and listening to port: " + port);
+
+      while(true){
+        Boolean isTerminated = false;
+        //ServerSocket.accept() outputs a socket object
+        socket = serverSocket.accept();
+        System.out.println("Client Connnected");
+
+        //Create an InputStream for the Socket
+
+        is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        String fromClient;
+
+        while(true){
+          fromClient = br.readLine();
+          System.out.println("Client sent: " + fromClient);
+          // Read command from client
+
+          switch(fromClient){
+
+            case "UPLD":
+              //calling UPLOAD method
+              System.out.println("Beginning procedure for uploading a file to the server...");
+              uploadFromClient();
+
+              break;
+
+            case "LIST":
+              //calling LIST method
+              listFilesOnServer();
+              break;
+
+            case "DWLD":
+              //calling DOWNLOAD method
+
+              break;
+
+            case "DELF":
+              //calling DELETE FILE method
+              deleteFileOnServer();
+              break;
+
+            case "QUIT":
+              //maybe put this in its own method?
+              System.out.println("Client terminated connection");
+              socket.close();
+              isTerminated = true;
+              break;
+
+            default: //"bad command"
+              System.out.println("Bad command");
+              break;
+          }//switch
+          if(isTerminated){
+            break;
+          }
+        }//while inner
+      }//while outer
+    }//try
+      catch(IOException e){
+        e.printStackTrace();
+        }
+  }//main
+}
+  /*
+  Main
+  Server opens port and goes into "wait for connection" state.
+  -Client connects
+
+
+  CONN
+  The client sends a request to the server to connect to it. The server accepts the connection
+  and waits for requests from the client.
+
+
+  UPLD
+
+  LIST
+
+  DWLD
+
+  DELF
+
+
+
+  QUIT
+1. Client sends operation (QUIT) to exit.
+2. Client closes socket, and exits.
+3. Server closes socket and goes back to "wait for operation from client" state.
+4. Client informs user that session has been closed.
+
+  */
